c47282c528433471b7384f1e7e1c98d67f60032d
lilarz <manasovarzuubek416@gmail.com>
2026-05-24 19:47:12 +0600

    fix db path and migrate

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=yandexgo.db"));
+    options.UseSqlite("Data Source=/tmp/yandexgo.db"));
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 {
@@ -22,12 +22,17 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-// ИЗМЕНИЛИ ЗДЕСЬ: теперь дефолтный экшен — Splash вместо Index
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Splash}/{id?}");
--- obj/Debug/net8.0/YandexGoClone.AssemblyInfo.cs
+++ obj/Debug/net8.0/YandexGoClone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YandexGoClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184ad0b542e8c7fd9d284551541a96ee1622227d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee1fa195b39ca1508da64ff51bb7655dedf46e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("YandexGoClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YandexGoClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
